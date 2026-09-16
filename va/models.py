5b0de353a62d9c1f8480caeadd4797e6a9d58061
BupeM <mmagrina616@gmail.com>
2026-09-16 11:52:06 +0300

    add VerbalAutopsy model

--- va/models.py
+++ va/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+#choices to be used in sex and place of death models
+SEX_CHOICES = [
+    ("M", "Male"),
+    ("F", "Female"),
+]
+
+PLACE_OF_DEATH_CHOICES = [
+    ("Hospital", "Hospital"),
+    ("Home", "Home"),
+    ("Other", "Other"),
+]
+
+# My models start here.
+class VerbalAutopsy(models.Model):
+    deceased_name = models.CharField(max_length = 120)
+    sex = models.CharField(max_length = 1, choices = SEX_CHOICES)
+    age_at_death = models.IntegerField()
+    date_of_death = models.DateField()
+    place_of_death = models.CharField(max_length = 100, choices = PLACE_OF_DEATH_CHOICES)
+    respondent_name = models.CharField(max_length = 120)
+    respondent_relationship = models.CharField(max_length = 50)
+    interview_date = models.DateField()
+    symptoms = models.TextField()
+    probable_cause_of_death = models.CharField(max_length = 100, blank = True) 
+    created_at = models.DateTimeField(auto_now_add=True)
